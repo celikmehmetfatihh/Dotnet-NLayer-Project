fce26fe46e91d3cf1218ccc9b65ca17d5c5d63e4
celikmehmetfatihh <celikmehmetfatihh@gmail.com>
2023-06-05 23:19:08 +0300

    Nlayer Application Updated

---
+++ Models/Score.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Models
+{
+    public class Score:Base
+    {
+        public String? score_type { get; set; }
+        public float score { get; set; }
+        public String? details { get; set; }
+
+        public float extraversion { get; set; }
+        public float agreeableness { get; set; }
+        public float openness { get; set; }
+        public float conscientiousness { get; set; }
+        public float neuroticism { get; set; }
+
+        public int VideoInterviewId { get; set; }
+
+        public VideoInterview VideoInterview { get; set; }
+    }
+}
---
+++ Models/VideoInterview.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Models
+{
+    public class VideoInterview:Base
+    {
+        public DateTime? date { get; set; }
+        public int? size { get; set; }
+        public String? format { get; set; }
+
+        public int UserId { get; set; } //foreign key 
+        public User User { get; set; }
+        public Score Score { get; set; }
+
+        public String? path { get; set; }
+    }
+}
